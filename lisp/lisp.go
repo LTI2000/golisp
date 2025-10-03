e78c3d6ff649c8353c98cd1407a34e33a8a07b41
LTI2000 <timo.koepke@googlemail.com>
2025-10-03 22:27:54 +0200

    define the seven primitives

--- lisp/lisp.go
+++ lisp/lisp.go
@@ -1,9 +1,40 @@
 package lisp
 
-import (
-	"fmt"
-)
+func Quote(x Value) Value {
+	return x
+}
 
-func Lisp() {
-	fmt.Println("Lisp")
+func Atom(x Value) Value {
+	if x.Atom() {
+		return Symbol("t")
+	} else {
+		return Symbol("nil")
+	}
+}
+
+func Eq(x Value, y Value) Value {
+	if x.Eq(y) {
+		return Symbol("t")
+	} else {
+		return Symbol("nil")
+	}
+}
+
+func Car(x Value) Value {
+	return x.Car()
+}
+
+func Cdr(x Value) Value {
+	return x.Cdr()
+}
+
+func Cons(x Value, y Value) Value {
+	return Pair(x, y)
+}
+
+func Cond(cs []Value) Value {
+	for _, c := range cs {
+		return Atom(c)
+	}
+	return Symbol("nil")
 }
