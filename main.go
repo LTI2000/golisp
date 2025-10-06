4cbc1999de1837184e87004701c6c78f1f88b45a
Timo Koepke <timo.koepke@vier.ai>
2025-10-06 11:54:12 +0200

    Remove obolsete comments

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
-// Go program to illustrate the
-// concept of main() function
-
-// Declaration of the main package
 package main
 
-// Importing packages
 import (
 	"github.com/LTI2000/golisp/lisp"
 )
 
-// Main function
 func main() {
-	lisp.Quote(lisp.Symbol("lisp"))
+	lisp.QUOTE(lisp.Symbol("lisp"))
 }
