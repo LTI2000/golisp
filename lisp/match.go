5018259b06a01761afa5d891c37808ac23ec68ab
LTI2000 <timo.koepke@googlemail.com>
2025-10-08 20:24:48 +0200

    Add Bindings to pattern matching

--- lisp/match.go
+++ lisp/match.go
@@ -1,40 +1,44 @@
 package lisp
 
 import (
+	"strings"
 	"unicode"
 )
 
-type Pattern interface {
-	Match(Value) bool
+// Bindings
+type Bindings struct {
+	parent *Bindings
+	name   Value
+	value  Value
 }
 
-type symbolPattern struct {
-	symbol Value
+func NewBindings() *Bindings {
+	return &Bindings{}
 }
 
-func (p *symbolPattern) Match(v Value) bool {
-	return p.symbol.IsEq(v)
+func (b *Bindings) Extend(name Value, value Value) *Bindings {
+	return &Bindings{b, name, value}
 }
 
-type variablePattern struct {
-	name Value
-}
+func (b *Bindings) String() string {
+	var sb strings.Builder
 
-func (p *variablePattern) Match(Value) bool {
-	return true
-}
-
-type pairPattern struct {
-	head Pattern
-	tail Pattern
-}
-
-func (p *pairPattern) Match(v Value) bool {
-	if v.IsAtom() {
-		return false
-	} else {
-		return p.head.Match(v.GetCar()) && p.tail.Match(v.GetCdr())
+	sb.WriteString("{")
+	for b.parent != nil {
+		sb.WriteString("[")
+		sb.WriteString(b.name.String())
+		sb.WriteString("=")
+		sb.WriteString(b.value.String())
+		sb.WriteString("]")
+		b = b.parent
 	}
+	sb.WriteString("}")
+	return sb.String()
+}
+
+// Pattern
+type Pattern interface {
+	Match(*Bindings, Value) (*Bindings, bool)
 }
 
 func NewPattern(pattern Value) Pattern {
@@ -46,6 +50,43 @@ func NewPattern(pattern Value) Pattern {
 		}
 	} else {
 		return &pairPattern{NewPattern(pattern.GetCar()), NewPattern(pattern.GetCdr())}
+	}
+}
+
+// symbolPattern
+type symbolPattern struct {
+	symbol Value
+}
+
+func (p *symbolPattern) Match(b *Bindings, v Value) (*Bindings, bool) {
+	return b, p.symbol.IsEq(v)
+}
+
+// variablePattern
+type variablePattern struct {
+	name Value
+}
+
+func (p *variablePattern) Match(b *Bindings, v Value) (*Bindings, bool) {
+	return b.Extend(p.name, v), true // FIXME extend bindings
+}
+
+// pairPattern
+type pairPattern struct {
+	head Pattern
+	tail Pattern
+}
+
+func (p *pairPattern) Match(b *Bindings, v Value) (*Bindings, bool) {
+	if v.IsAtom() {
+		return b, false
+	} else {
+		if b1, matches := p.head.Match(b, v.GetCar()); matches {
+			if b2, matches := p.tail.Match(b1, v.GetCdr()); matches {
+				return b2, true
+			}
+		}
+		return b, false
 	}
 }
 
--- lisp/match_test.go
+++ lisp/match_test.go
@@ -7,25 +7,33 @@ func TestMatchSymbol(t *testing.T) {
 		t.Fatalf("err %v", err)
 	} else if value, err := Read("nil"); err != nil {
 		t.Fatalf("err %v", err)
-	} else if expected, actual := true, NewPattern(pattern).Match(value); expected != actual {
+	} else if bindings, matches := NewPattern(pattern).Match(NewBindings(), value); !matches {
+		t.Errorf("expected match: %v %v", pattern, value)
+	} else if expected, actual := "{}", bindings.String(); expected != actual {
 		t.Errorf("expected %v, actual %v", expected, actual)
 	}
 }
+
 func TestMatchPair(t *testing.T) {
 	if pattern, err := Read("(a b)"); err != nil {
 		t.Fatalf("err %v", err)
 	} else if value, err := Read("(a . (b . ()))"); err != nil {
 		t.Fatalf("err %v", err)
-	} else if expected, actual := true, NewPattern(pattern).Match(value); expected != actual {
+	} else if bindings, matches := NewPattern(pattern).Match(NewBindings(), value); !matches {
+		t.Errorf("expected match: %v %v", pattern, value)
+	} else if expected, actual := "{}", bindings.String(); expected != actual {
 		t.Errorf("expected %v, actual %v", expected, actual)
 	}
 }
+
 func TestMatchVariable(t *testing.T) {
 	if pattern, err := Read("X"); err != nil {
 		t.Fatalf("err %v", err)
 	} else if value, err := Read("t"); err != nil {
 		t.Fatalf("err %v", err)
-	} else if expected, actual := true, NewPattern(pattern).Match(value); expected != actual {
+	} else if bindings, matches := NewPattern(pattern).Match(NewBindings(), value); !matches {
+		t.Errorf("expected match: %v %v", pattern, value)
+	} else if expected, actual := "{[X=t]}", bindings.String(); expected != actual {
 		t.Errorf("expected %v, actual %v", expected, actual)
 	}
 }
@@ -35,16 +43,21 @@ func TestMatchVariablePair1(t *testing.T) {
 		t.Fatalf("err %v", err)
 	} else if value, err := Read("t"); err != nil {
 		t.Fatalf("err %v", err)
-	} else if expected, actual := true, !NewPattern(pattern).Match(value); expected != actual {
+	} else if bindings, matches := NewPattern(pattern).Match(NewBindings(), value); matches {
+		t.Errorf("expected non-match: %v %v", pattern, value)
+	} else if expected, actual := "{}", bindings.String(); expected != actual {
 		t.Errorf("expected %v, actual %v", expected, actual)
 	}
 }
+
 func TestMatchVariablePair2(t *testing.T) {
 	if pattern, err := Read("(X)"); err != nil {
 		t.Fatalf("err %v", err)
-	} else if value, err := Read("(t)"); err != nil {
+	} else if value, err := Read("(x)"); err != nil {
 		t.Fatalf("err %v", err)
-	} else if expected, actual := true, NewPattern(pattern).Match(value); expected != actual {
+	} else if bindings, matches := NewPattern(pattern).Match(NewBindings(), value); !matches {
+		t.Errorf("expected match: %v %v", pattern, value)
+	} else if expected, actual := "{[X=x]}", bindings.String(); expected != actual {
 		t.Errorf("expected %v, actual %v", expected, actual)
 	}
 }
