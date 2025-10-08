ed5ebe9de8d6182d62471a93d94368af945246ff
LTI2000 <timo.koepke@googlemail.com>
2025-10-08 19:17:38 +0200

    Add TestCompose

--- lisp/util_test.go
+++ lisp/util_test.go
@@ -1,17 +1,54 @@
 package lisp
 
-//import "testing"
+import (
+	"errors"
+	"testing"
+)
 
-// func TestCompose(t *testing.T) {
-// 	f, g := add1, length
-// 	h := Compose(f, g)
-// 	n, l, err := h(1, "23")
-// }
+var err0 = errors.New("e0")
+var err1 = errors.New("e1")
+var f0 = func(x float64) (float64, error) {
+	return x * 2.0, nil
+}
+var f1 = func(x float64) (float64, error) {
+	return 0.0, err0
+}
+var g0 = func(s string) (int, error) {
+	return len(s), nil
+}
+var g1 = func(s string) (int, error) {
+	return 0, err1
+}
 
-// func add1(n int) (int, error) {
-// 	return n + 1, nil
-// }
-
-// func length(s string) (int, error) {
-// 	return len(s), nil
-// }
+func TestCompose(t *testing.T) {
+	{
+		x, l, err := Compose(f0, g0)(1.0, "23")
+		if expected, actual := 2.0, x; expected != actual {
+			t.Errorf("expected %v, actual %v", expected, actual)
+		}
+		if expected, actual := 2, l; expected != actual {
+			t.Errorf("expected %v, actual %v", expected, actual)
+		}
+		if expected, actual := error(nil), err; expected != actual {
+			t.Errorf("expected %v, actual %v", expected, actual)
+		}
+	}
+	{
+		_, _, err := Compose(f1, g0)(1.0, "23")
+		if expected, actual := err0, err; expected != actual {
+			t.Errorf("expected %v, actual %v", expected, actual)
+		}
+	}
+	{
+		_, _, err := Compose(f0, g1)(1.0, "23")
+		if expected, actual := err1, err; expected != actual {
+			t.Errorf("expected %v, actual %v", expected, actual)
+		}
+	}
+	{
+		_, _, err := Compose(f1, g1)(1.0, "23")
+		if expected, actual := err0, err; expected != actual {
+			t.Errorf("expected %v, actual %v", expected, actual)
+		}
+	}
+}
