0abdc5e4ec95f799789f53ff53c917418e9bef8c
Timo Koepke <timo.koepke@vier.ai>
2025-10-08 18:17:22 +0200

    Add Compose function

--- lisp/util.go
+++ lisp/util.go
@@ -5,3 +5,18 @@ import "strings"
 func Read(source string) (Value, error) {
 	return NewReader(NewScanner(strings.NewReader(source))).ReadValue()
 }
+
+func Compose[A1, B1, A2, B2 any](f func(A1) (B1, error), g func(A2) (B2, error)) func(A1, A2) (B1, B2, error) {
+	var b1 B1
+	var b2 B2
+	var err error = nil
+	return func(a1 A1, a2 A2) (B1, B2, error) {
+		if err == nil {
+			b1, err = f(a1)
+		}
+		if err == nil {
+			b2, err = g(a2)
+		}
+		return b1, b2, err
+	}
+}
---
+++ lisp/util_test.go
@@ -0,0 +1,17 @@
+package lisp
+
+//import "testing"
+
+// func TestCompose(t *testing.T) {
+// 	f, g := add1, length
+// 	h := Compose(f, g)
+// 	n, l, err := h(1, "23")
+// }
+
+// func add1(n int) (int, error) {
+// 	return n + 1, nil
+// }
+
+// func length(s string) (int, error) {
+// 	return len(s), nil
+// }
