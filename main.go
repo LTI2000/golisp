0dd7005ae0b834f8fb4fbc1fd8eec11c5b71d3da
LTI2000 <timo.koepke@googlemail.com>
2025-10-11 23:34:29 +0200

    Add basic repl

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"os"
+
 	"github.com/LTI2000/golisp/lisp"
 )
 
 func main() {
-	lisp.COND(lisp.List(lisp.Nil, lisp.T), lisp.List(lisp.T, lisp.Nil))
+	fmt.Printf("; welcome to golisp\n")
+	repl(os.Stdin)
+}
+
+func repl(r io.Reader) {
+	scanner := lisp.NewScanner(r)
+	reader := lisp.NewReader(scanner)
+	for {
+		if value, err := reader.ReadValue(); err != nil {
+			fmt.Printf("read failed: %v\n", err.Error())
+			return
+		} else if expression, err := lisp.ParseExpression(value); err != nil {
+			fmt.Printf("parse failed: %v\n", err.Error())
+			return
+		} else if v, err := lisp.Eval(expression); err != nil {
+			fmt.Printf("eval failed: %v\n", err.Error())
+			return
+		} else {
+			fmt.Printf("; %v\n", v)
+		}
+	}
 }
