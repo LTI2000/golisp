208f2c8a1ec660e84e3c68fc7c205142bf39bcdb
Timo Koepke <timo.koepke@vier.ai>
2025-10-22 16:33:56 +0200

    Fix metacircular interpreter and tests

--- eval_test.lisp
+++ eval_test.lisp
@@ -1,1 +1,9 @@
-(eval. 'a '())
+(eval. '(quote a) '())
+
+(eval. '((label subst 
+                (lambda (x y z)
+                  (cond ((atom z)
+                         (cond ((eq z y) x)
+                               ('t z)))
+                        ('t (cons (subst x y (car z))
+                                  (subst x y (cdr z))))))) 'm 'b '(a b (a b c) d)) '())
