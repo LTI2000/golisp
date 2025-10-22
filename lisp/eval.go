cc9577f3b04a5d454b90fb7bedc9a23dee83d997
LTI2000 <timo.koepke@googlemail.com>
2025-10-22 21:41:09 +0200

    improve names in eval.go

--- lisp/eval.go
+++ lisp/eval.go
@@ -46,102 +46,102 @@ func assoc(x, y Expression) (Expression, error) {
 	}
 }
 
-func eval(e, a Expression) (Expression, error) {
-	if ok := Match0("X:atom", e); ok {
-		return assoc(e, a)
-	} else if x, ok := Match1("(quote X)", e, "X"); ok {
+func eval(exp, env Expression) (Expression, error) {
+	if ok := Match0("X:atom", exp); ok {
+		return assoc(exp, env)
+	} else if x, ok := Match1("(quote X)", exp, "X"); ok {
 		return x, nil
-	} else if x, ok := Match1("(atom X)", e, "X"); ok {
-		if a0, err := eval(x, a); err != nil {
+	} else if x, ok := Match1("(atom X)", exp, "X"); ok {
+		if a0, err := eval(x, env); err != nil {
 			return nil, err
 		} else {
 			return Bool(Atom(a0)), nil
 		}
-	} else if x, y, ok := Match2("(eq X Y)", e, "X", "Y"); ok {
-		if a0, err := eval(x, a); err != nil {
+	} else if x, y, ok := Match2("(eq X Y)", exp, "X", "Y"); ok {
+		if a0, err := eval(x, env); err != nil {
 			return nil, err
-		} else if b0, err := eval(y, a); err != nil {
+		} else if b0, err := eval(y, env); err != nil {
 			return nil, err
 		} else {
 			return Bool(Eq(a0, b0)), nil
 		}
-	} else if x, ok := Match1("(car X)", e, "X"); ok {
-		if a0, err := eval(x, a); err != nil {
+	} else if x, ok := Match1("(car X)", exp, "X"); ok {
+		if a0, err := eval(x, env); err != nil {
 			return nil, err
 		} else {
 			return Car(a0)
 		}
-	} else if x, ok := Match1("(cdr X)", e, "X"); ok {
-		if a0, err := eval(x, a); err != nil {
+	} else if x, ok := Match1("(cdr X)", exp, "X"); ok {
+		if a0, err := eval(x, env); err != nil {
 			return nil, err
 		} else {
 			return Cdr(a0)
 		}
-	} else if x, y, ok := Match2("(cons X Y)", e, "X", "Y"); ok {
-		if a0, err := eval(x, a); err != nil {
+	} else if x, y, ok := Match2("(cons X Y)", exp, "X", "Y"); ok {
+		if a0, err := eval(x, env); err != nil {
 			return nil, err
-		} else if b0, err := eval(y, a); err != nil {
+		} else if b0, err := eval(y, env); err != nil {
 			return nil, err
 		} else {
 			return Cons(a0, b0), nil
 		}
-	} else if c, ok := Match1("(cond . C:list)", e, "C"); ok {
-		return evcon(c, a)
-	} else if x, y, ok := Match2("(X:atom . Y:list)", e, "X", "Y"); ok {
-		if f, err := assoc(x, a); err != nil {
+	} else if c, ok := Match1("(cond . C:list)", exp, "C"); ok {
+		return evcon(c, env)
+	} else if x, y, ok := Match2("(X:atom . Y:list)", exp, "X", "Y"); ok {
+		if f, err := assoc(x, env); err != nil {
 			return nil, err
 		} else {
-			return eval(Cons(f, y), a)
+			return eval(Cons(f, y), env)
 		}
-	} else if n, f, p, ok := Match3("((label N F) . P:list)", e, "N", "F", "P"); ok {
-		return eval(Cons(f, p), Cons(List(n, Must(Car, e)), a))
-	} else if p, b, x, ok := Match3("((lambda P B) . X:list)", e, "P", "B", "X"); ok {
-		if v0, err := evlis(x, a); err != nil {
+	} else if n, f, p, ok := Match3("((label N F) . P:list)", exp, "N", "F", "P"); ok {
+		return eval(Cons(f, p), Cons(List(n, Must(Car, exp)), env))
+	} else if p, b, x, ok := Match3("((lambda P B) . X:list)", exp, "P", "B", "X"); ok {
+		if v0, err := evlis(x, env); err != nil {
 			return nil, err
 		} else if v1, err := pair(p, v0); err != nil {
 			return nil, err
-		} else if v2, err := Append(v1, a); err != nil {
+		} else if v2, err := Append(v1, env); err != nil {
 			return nil, err
 		} else {
 			return eval(b, v2)
 		}
 	} else {
-		return nil, fmt.Errorf("eval: illegal expressoin: %v", e)
+		return nil, fmt.Errorf("eval: illegal expressoin: %v", exp)
 	}
 }
 
-func evcon(c, a Expression) (Expression, error) {
-	if car_c, cdr_c, err := Uncons(c, "evcon1"); err != nil {
+func evcon(clauses, env Expression) (Expression, error) {
+	if car_c, cdr_c, err := Uncons(clauses, "evcon1"); err != nil {
 		return nil, err
 	} else if caar_c, cdar_c, err := Uncons(car_c, "evcon2"); err != nil {
 		return nil, err
-	} else if x, err := eval(caar_c, a); err != nil {
+	} else if x, err := eval(caar_c, env); err != nil {
 		return nil, err
 	} else if x != NIL {
 		if cadar_c, _, err := Uncons(cdar_c, "evcon3"); err != nil {
 			return nil, err
 		} else {
-			return eval(cadar_c, a)
+			return eval(cadar_c, env)
 		}
 	} else {
-		return evcon(cdr_c, a)
+		return evcon(cdr_c, env)
 	}
 }
 
-func evlis(m, a Expression) (Expression, error) {
-	if m == NIL {
+func evlis(exps, env Expression) (Expression, error) {
+	if exps == NIL {
 		return NIL, nil
-	} else if car_m, cdr_m, err := Uncons(m, "evlis1"); err != nil {
+	} else if car_m, cdr_m, err := Uncons(exps, "evlis1"); err != nil {
 		return nil, err
-	} else if first, err := eval(car_m, a); err != nil {
+	} else if first, err := eval(car_m, env); err != nil {
 		return nil, err
-	} else if rest, err := evlis(cdr_m, a); err != nil {
+	} else if rest, err := evlis(cdr_m, env); err != nil {
 		return nil, err
 	} else {
 		return Cons(first, rest), nil
 	}
 }
 
-func Eval(e, a Expression) (Expression, error) {
-	return eval(e, a)
+func Eval(exp, env Expression) (Expression, error) {
+	return eval(exp, env)
 }
