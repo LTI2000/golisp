0ec03c9c3f7babb1d22339c72d39843a296429e7
Timo Koepke <timo.koepke@vier.ai>
2025-10-23 10:59:43 +0200

    compare actual test output with expected output

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 go clean
 go build
 go test ./...
-cat test.lisp eval.lisp eval_test.lisp | go run main.go
+cat test.lisp eval.lisp eval_test.lisp | go run main.go > actual.txt
+diff expected.txt actual.txt
